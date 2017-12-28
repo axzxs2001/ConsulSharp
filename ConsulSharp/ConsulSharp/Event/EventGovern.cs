8056624027e001d86a932ed4342781d9f0c8f152
桂素伟 <axzxs2001@163.com>
2017-12-29 07:55:00 +0800

    Event操作完成

--- ConsulSharp/ConsulSharp/Event/EventGovern.cs
+++ ConsulSharp/ConsulSharp/Event/EventGovern.cs
@@ -26,10 +26,36 @@ namespace ConsulSharp
         /// </summary>
         /// <param name="firEventParmeter">in parmeter</param>
         /// <returns></returns>
-        public async Task<(bool result, FireEvent fireEvent)> FireEvent(FireEventParmeter firEventParmeter)
+        public async Task<(bool result, FireEvent fireEvent)> FireEvent(FireEventParmeter fireEventParmeter)
         {
-            return await Put<FireEventParmeter, FireEvent>(firEventParmeter, $"/event/fire");
+            return await Put<FireEventParmeter, FireEvent>(fireEventParmeter, $"/event/fire");
         }
+        /// <summary>
+        /// Fire Event
+        /// </summary>
+        /// <param name="firEventParmeter">in parmeter</param>
+        /// <returns></returns>
+        public async Task<FireEvent[]> FireEvent(ListEventParmeter listEventParmeter)
+        {
+            var parmeter = new StringBuilder();
+            if(!string.IsNullOrEmpty(listEventParmeter.Name))
+            {
+                parmeter.Append($"&name={listEventParmeter.Name}");
+            }
+            if (!string.IsNullOrEmpty(listEventParmeter.Node))
+            {
+                parmeter.Append($"&node={listEventParmeter.Node}");
+            }
+            if (!string.IsNullOrEmpty(listEventParmeter.Service))
+            {
+                parmeter.Append($"&node={listEventParmeter.Service}");
+            }
+            if (!string.IsNullOrEmpty(listEventParmeter.Tag))
+            {
+                parmeter.Append($"&node={listEventParmeter.Tag}");
+            }
 
+            return await Get<FireEvent[]>($"/event/fire?{parmeter.ToString().TrimStart('&')}");
+        }
     }
 }
---
+++ ConsulSharp/ConsulSharp/Event/ListEventParmeter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsulSharp
+{
+    /// <summary>
+    /// List Event
+    /// </summary>
+    public class ListEventParmeter
+    {
+        /// <summary>
+        /// name
+        /// </summary>
+        public string Name { get; set; }    
+
+        /// <summary>
+        /// node
+        /// </summary>
+        public string Node { get; set; }
+
+        /// <summary>
+        /// service
+        /// </summary>
+        public string Service { get; set; }
+
+        /// <summary>
+        /// tag
+        /// </summary>
+        public string Tag { get; set; }
+    }
+}
